d82faabb69359c0d62adb322d7a9811fdf3afbab
Daniel Barreto <danielf.barreto@javeriana.edu.co>
2021-09-28 16:01:30 -0500

    añadi los archivos necesarios para iniciar con el proyecto

---
+++ src/commandCenter.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <unistd.h>
+#include "book.h"
+
+int main(int argc, char *argv[]) {
+
+  //abre el pipe verde
+  //crea el pipe personal y manda la info de la solicitud
+  //manda info y espera la respuesta
+
+  return 0;
+}
