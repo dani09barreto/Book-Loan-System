20678e11cabb29c28598a73edf1e796ca4e2232d
Daniel Barreto <danielf.barreto@javeriana.edu.co>
2021-09-28 16:40:05 -0500

    elimine archivos inecesarios

--- code.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(int argc, char const *argv[]){
-    
-    printf("hola mundo");
-    return 0;
-}
--- pruebaCarlos.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(int argc, char const *argv[]){
-    
-    printf("hola mundo");
-    printf("lo logre");
-    return 0;
-}
