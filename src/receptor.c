ce772fd43787c7448fb2c18d130b4d899cd092fe
Daniel Barreto <danielf.barreto@javeriana.edu.co>
2021-09-29 10:21:16 -0500

    Cambios en receptor

--- src/receptor.c
+++ src/receptor.c
@@ -9,6 +9,58 @@
 #include <string.h>
 #include "book.h"
 
+
+void requestBook (char *fileSecondpipe, int fd){
+
+    do{
+        if ((fd = open (fileSecondpipe, O_WDONLY)) == -1){
+            perror("Receptor abriendo el pipe respuesta\n");
+            perror("Se intentara mas tarde\n");
+            sleep(5);
+        }
+        else{
+            make = 1;
+        }
+    }while(make == 0);
+    printf("Proceso receptor envia respuesta a pipe del proceso solicitud\n");
+    write(fd, 1, sizeof(int));
+    close (fd);
+}
+
+void returnBook (char *fileSecondpipe, int fd){
+
+    do{
+        if ((fd = open (fileSecondpipe, O_WDONLY)) == -1){
+            perror("Receptor abriendo el pipe respuesta\n");
+            perror("Se intentara mas tarde\n");
+            sleep(5);
+        }
+        else{
+            make = 1;
+        }
+    }while(make == 0);
+    printf("Proceso receptor envia respuesta a pipe del proceso solicitud\n");
+    write(fd, 1, sizeof(int));
+    close (fd);
+}
+
+void renovateBook (char *fileSecondpipe, int fd){
+
+    do{
+        if ((fd = open (fileSecondpipe, O_WDONLY)) == -1){
+            perror("Receptor abriendo el pipe respuesta\n");
+            perror("Se intentara mas tarde\n");
+            sleep(5);
+        }
+        else{
+            make = 1;
+        }
+    }while(make == 0);
+    printf("Proceso receptor envia respuesta a pipe del proceso solicitud\n");
+    write(fd, 1, sizeof(int));
+    close (fd);
+}
+
 int main(int argc, char *argv[]){
 
     //codigo del servidor
@@ -23,9 +75,9 @@ int main(int argc, char *argv[]){
 
     int fd, fd1, pid, n, bytes, res, make = 0;
     char *filepipe = argv[2];
-    book peticion;
+    book request;
     
-    //crea el pipe receptor lo abre
+    //crea el pipe receptor y lo abre
     mode_t fifo_mode = S_IRUSR | S_IWURS;
 
     unlink(filepipe);
@@ -40,61 +92,34 @@ int main(int argc, char *argv[]){
     }
     printf("Se abrio el pipe receptor\n");
 
-    bytes = read (fp, &peticion, sizeof(book));
+    // lee lo que commandCenter le manda y lo guarda en request
+    bytes = read (fp, &request, sizeof(book));
 
     if (bytes == -1){
         perror ("Error al leer el pipe");
         exit (0);
     }
     printf("El proceso receptor recibio la solicitud\n");
-    switch (peticion.operation){
+
+    //Identifica cual es el proceso a realizar D: Devolver, P: Pedir, R: Renovar
+
+    switch (request.operation){
 
     case 'P':
-        do{
-            if ((fd1 = open (peticion.secondpipe, O_WDONLY)) == -1){
-                perror("Receptor abriendo el pipe respuesta\n");
-                perror("Se intentara mas tarde\n");
-                sleep(5);
-            }
-            else{
-                make = 1;
-            }
-        }while(make == 0);
-        printf("Proceso receptor envia respuesta\n");
-        write(fd1, 1, sizeof(int));
+        requestBook(&request.secondpipe, fd1);
         break;
     case 'D':
-            do{
-            if ((fd1 = open (peticion.secondpipe, O_WDONLY)) == -1){
-                perror("Receptor abriendo el pipe respuesta\n");
-                perror("Se intentara mas tarde\n");
-                sleep(5);
-            }
-            else{
-                make = 1;
-            }
-        }while(make == 0);
-        printf("Proceso receptor envia respuesta\n");
-        write(fd1, 1, sizeof(int));
+        returnBook(&request.secondpipe, fd1);
         break;
     case 'R':
-            do{
-            if ((fd1 = open (peticion.secondpipe, O_WDONLY)) == -1){
-                perror("Receptor abriendo el pipe respuesta\n");
-                perror("Se intentara mas tarde\n");
-                sleep(5);
-            }
-            else{
-                make = 1;
-            }
-        }while(make == 0);
-        printf("Proceso receptor envia respuesta\n");
-        write(fd1, 1, sizeof(int));
+        returnBook(&request.secondpipe, fd1);
         break;
     default:
         printf("Accion no se puede realizar\n");
         break;
     }
+
+    printf("receptor a termindado")
 
     return 0;
 }
