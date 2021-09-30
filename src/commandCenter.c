aef362f97b732f82c5f5cb8aaf86bf690bac31b1
dani09barreto <danielf.barreto@javeriana.edu.co>
2021-09-30 14:27:11 -0500

    Funcionalidad de comunicacion entre pipes

--- src/commandCenter.c
+++ src/commandCenter.c
@@ -7,11 +7,60 @@
 #include <unistd.h>
 #include <unistd.h>
 #include "book.h"
+void createPipe(char *receptorPipe , book bookRequest){
+  int fd;
+  int create = 0;
+  int verify;
+  mode_t fifo_mode = S_IRUSR | S_IWUSR;
 
+  do{
+    fd = open(receptorPipe,O_RDONLY);
+    if(fd==-1){
+      perror("[!] Error en la lectura del pipe");
+      sleep(10);
+    }else
+      create = 1;
+  }while(create != 1);
+
+  unlink(bookRequest.secondpipe); 
+
+  if (mkfifo (bookRequest.secondpipe, fifo_mode) == -1) {
+    perror("[!] Request mkfifo");
+    exit(1);
+  }  
+
+  write(fd, &bookRequest, sizeof(book));
+  create = 0;
+
+  do { 
+    if ((fd = open(bookRequest.secondpipe, O_RDONLY)) == -1) {
+      perror("[!] Cliente  Abriendo el segundo pipe. Se volvera a intentar ");
+      sleep(5);
+    } else create = 1; 
+  } while (create !=1);
+  
+  if(read(fd, &create, 10) == -1){
+    perror ("[!] No se pudo leer la respuesta del PS receptor");
+    exit(0);
+  }
+  if(create == 1){
+    printf("Su peticion fue exitosa ");
+  }else{
+    printf("Su peticion no fue exitosa");
+  }
+
+}
 int main(int argc, char *argv[]) {
 
+  if (argc != 5){
+    perror("Numero de argumentos invalidos\n");
+    printf("ej: ./center –i file –p fileReceptor \n");
+    exit (0);
+  }
+  book bookRequest;  
   //abre el pipe verde
-  //crea el pipe personal y manda la info de la solicitud
+  createPipe(argv[1],bookRequest);
+    //crea el pipe personal y manda la info de la solicitud
   //manda info y espera la respuesta
 
   return 0;
