dbdfbd69be4f7d4c7820ddb746aace59bb384eb1
Daniel Barreto <danielf.barreto@javeriana.edu.co>
2021-10-05 21:31:28 -0500

    Lectura BD

--- src/book.h
+++ src/book.h
@@ -2,6 +2,8 @@
 #define MAXBOOK 10000
 #define MAXNAME 50
 #define TAMMENSAJE 10
+#define MAXNUM 10
+#define MAXLINE 1000
 
 typedef struct Book{
     char operation;
@@ -9,4 +11,28 @@ typedef struct Book{
     int ISBN;
     char secondpipe[MAXNAME];
 }book;
+
+typedef struct Date{
+    int day;
+    int month;
+    int year;
+}date;
+
+typedef struct Request{
+    int stock;
+    char operation;
+    date initialDate;
+}request;
+
+typedef struct Data{
+    char name [MAXNAME];
+    int ISBN;
+    int stocks;
+    request requests [MAXNUM];
+}data;
+
+
+
+
+
 
