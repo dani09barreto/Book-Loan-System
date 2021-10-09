95e3494559a085b34cbf3a2d78452dc6717348f7
Daniel Barreto <danielf.barreto@javeriana.edu.co>
2021-10-09 16:24:23 -0500

    Conexion de procesos terminada

--- src/code/receptor.c
+++ src/code/receptor.c
@@ -10,6 +10,7 @@
 data dataBase [MAXBOOK];
 int posData = 0;
 int posRequest = 0;
+date dateDefault;
 
 int tokDataRequest (char *line, int stoks){
 
@@ -28,12 +29,15 @@ int tokDataRequest (char *line, int stoks){
       }
       if (i == 2){
          dataBase[posData - 1].requests[posRequest].initialDate.day = atoi(token);
+         dateDefault.day = atoi(token);
       }
       if (i == 3){
          dataBase[posData - 1].requests[posRequest].initialDate.month = atoi(token);
+         dateDefault.month = atoi(token);
       }
       if (i == 4){
          dataBase[posData - 1].requests[posRequest].initialDate.year = atoi(token);
+         dateDefault.year = atoi(token);
       }
    }
    posRequest ++;
@@ -164,41 +168,54 @@ int main (int argc, char *argv[]){
    }
 
    readDataBase(argv[4]);
+   printf("\tDate default: %d-%d-%d", dateDefault.day, dateDefault.month, dateDefault.year);
    printf("\n");
    printf("\tSe leyo de la BD\n");
+   printf("\t----------------\n");
    for (int i = 0; i < posData; i ++){
       printf ("\t%s %i %i\n", dataBase[i].name, dataBase[i].ISBN, dataBase[i].stocks);
       for(int j = 0; j < dataBase[i].stocks; j ++){
          printf ("\t%i,%c,%i-%i-%i\n", dataBase[i].requests[j].stock, dataBase[i].requests[j].operation, dataBase[i].requests[j].initialDate.day, dataBase[i].requests[j].initialDate.month, dataBase[i].requests[j].initialDate.year);
       }
    }
-   
+
+   printf("\n");
+   printf("\t---------------------------------------------\n");
+   printf("\tSe crea el pipe %s para recibir solicitud\n", argv[2]);
+   printf("\t---------------------\n");
+   unlink(argv[2]);
+   if (mkfifo (argv[2], fifo_mode) == -1) {
+      perror("\tReceptor mkfifo");
+      exit(1);
+   }
+   printf("\tSe esta abriendo el pipe %s para recibir solicitud\n", argv[2]);
+   printf("\t---------------------\n");
+   if ((fd = open(argv[2], O_RDONLY)) == -1){
+      perror("\tError al abrir el pipe\n");
+      exit (0);
+   }
+
+   printf("\tSe abrio %s \n", argv[2]);
+   printf("\t---------------------\n");
+
    do{
-      printf("\n");
-      printf("\t---------------------------------------------\n");
-      printf("\tSe crea el pipe %s para recibir solicitud\n", argv[2]);
-      printf("\t---------------------\n");
-      unlink(argv[2]);
-      if (mkfifo (argv[2], fifo_mode) == -1) {
-         perror("\tReceptor mkfifo");
-         exit(1);
-      }
-      printf("\tSe esta abriendo el pipe %s para recibir solicitud\n", argv[2]);
-      printf("\t---------------------\n");
-      if ((fd = open(argv[2], O_RDONLY)) == -1){
-         perror("\tError al abrir el pipe\n");
-         exit (0);
-      }
-
-      printf("\tSe abrio %s \n", argv[2]);
-      printf("\t---------------------\n");
+      
+      printf("\t---------------------------\n");      
       printf("\tSe esta leyendo la solicitud que manda el PS\n"); 
-
       bytes = read (fd, &bookRequest, sizeof(bookRequest));
+      if (bytes == 0){
+         printf("\t---------------------------\n");
+         printf("\tAcabo el proceso se esperara 40 segundos para que genere una solicitud mas\n");
+         sleep(40);
+         bytes = read (fd, &bookRequest, sizeof(bookRequest));
+         if (bytes == 0)
+            break;
+      }
       if (bytes == -1) {
          perror("\tproceso lector:");
          exit(1);
       }
+      printf("\t---------------------------\n");
       printf("\tSe lee:\n");
       printf("\tSolicitud: %c\n", bookRequest.operation);
       printf("\tNombre: %s\n", bookRequest.name);
@@ -228,8 +245,10 @@ int main (int argc, char *argv[]){
          printf("\tAccion no se puede realizar\n");
          break;
       }
+
    }while(bytes > 0);
-   
+   printf("\t------------------------\n");
+   printf("\tel proceso termino\n");
    exit(0);
 }
 
